9cd74403f2b1f73583bba47a86c63e79eb5602d9
mvanikiotis1 <mvanikiotis1@babson.edu>
2021-12-10 18:48:15 -0500

    update

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ consumer_key = os.environ['TWITTER_API_KEY']
 consumer_secret = os.environ['TWITTER_API_SECRET']
 bearer_token = os.environ['TWITTER_BEARER']
 
+accountSID = os.environ['accountSID']
+authToken = os.environ['authToken']
+myTwilioNumber = os.environ['myTwilioNumber']
